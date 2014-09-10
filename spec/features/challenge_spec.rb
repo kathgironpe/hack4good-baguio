806541f844716a3ec6eb209e0578fc470bceddb2
Katherine G. Pe <bridgeutopia@gmail.com>
2014-09-10 09:56:16 +0800

    Fix failing spec for challenge

--- spec/features/challenge_spec.rb
+++ spec/features/challenge_spec.rb
@@ -3,14 +3,15 @@ require 'rails_helper'
 feature 'Manage Challenge' do
   let(:admin) { create(:user, admin: true) }
   let(:user) { create(:user, admin: false) }
-  let(:challenge_partner) { create(:challenger_partner) }
+  let!(:challenge_partner) { create(:challenge_partner) }
 
   scenario 'Admin creates a new challenge' do
     sign_in(admin)
     expect(page).to have_content('Challenge Partners')
     click_link 'Challenges'
+    expect(page).to have_content('New Challenge')
     click_link 'New Challenge'
-    fill_in 'challenge_challenger_partner_id', with: challenge_partner.id
+    select challenge_partner.name, from: 'challenge_challenge_partner_id'
     fill_in 'challenge_title', with: 'Title'
     fill_in 'challenge_description', with: 'Description'
     click_button 'Create Challenge'
